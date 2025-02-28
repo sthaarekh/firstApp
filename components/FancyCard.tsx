c3feee7f0c52fa678b34b6fab252daf0f1ab0d4d
sthaarekh <arekhshrestha@gmail.com>
2025-02-28 23:18:02 +0545

    Added Image

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const FancyCard = () => {
   return (
     <View>
-      <Text style={styles.headingText}>FancyCard</Text>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image source={{
+            uri: 'https://images.squarespace-cdn.com/content/v1/5b735348266c075124b0ffb3/1568373828844-AP1ZPOIWBGGFJWMIZ55N/Boudha_Stupa_181030-9.jpg'
+        }}
+        style = {styles.cardImage}></Image>
+      </View>
     </View>
   )
 }
@@ -17,5 +23,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         paddingHorizontal: 10,
         color: 'white'
+    },
+    card:{},
+    cardElevated:{},
+    cardImage:{
+        height: 180,
+        
     },
 })
