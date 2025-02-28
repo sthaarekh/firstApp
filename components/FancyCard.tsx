0983fc14f4b75df5b7b8d53845307b0d097d3672
sthaarekh <arekhshrestha@gmail.com>
2025-02-28 23:23:01 +0545

    Card created successfully

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -10,6 +10,12 @@ const FancyCard = () => {
             uri: 'https://images.squarespace-cdn.com/content/v1/5b735348266c075124b0ffb3/1568373828844-AP1ZPOIWBGGFJWMIZ55N/Boudha_Stupa_181030-9.jpg'
         }}
         style = {styles.cardImage}></Image>
+        <View style={styles.cardBody}>
+            <Text style={styles.cardTitle}>Boudhanath</Text>
+            <Text style={styles.cardLabel}>Kathmandu, Bagmati Province</Text>
+            <Text style={styles.cardDescription}>Boudha Stupa; or Jarung Kashor, also known as Khasti Chaitya or Khāsa Chaitya, is a stupa and major spiritual landmark seen as the embodiment of the enlightened mind of all the Buddhas, located in Boudhanath, within the city of Kathmandu, Nepal.</Text>
+            <Text style={styles.cardFooter}>12 mins away</Text>
+            </View>
       </View>
     </View>
   )
@@ -30,4 +36,9 @@ const styles = StyleSheet.create({
         height: 180,
         
     },
+    cardBody:{},
+    cardTitle:{},
+    cardLabel:{},
+    cardDescription:{},
+    cardFooter:{},
 })
