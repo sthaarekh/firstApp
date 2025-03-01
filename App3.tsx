b1e42b51d671de39360578c7e8aeee0877e736f6
sthaarekh <arekhshrestha@gmail.com>
2025-03-01 15:06:28 +0545

    Logic to generate passwords

--- App3.tsx
+++ App3.tsx
@@ -20,7 +20,12 @@ export default function App3() {
         //
     }
     const createPassword = (characters : string, passwordLength : number)=>{
-
+        let result =''
+        for (let index = 0; index < passwordLength; index++) {
+            const characterIndex = Math.round(Math.random() * characters.length)
+            result += characters.charAt(characterIndex)
+        }
+        return result;
     }
     const resetPasswordState = ()=>{
 
