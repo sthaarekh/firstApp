a3750cd41ce8dc03d493d189ac6ba4c4a5ea7cf0
sthaarekh <arekhshrestha@gmail.com>
2025-03-01 14:48:22 +0545

    Yup package initiated

--- App3.tsx
+++ App3.tsx
@@ -1,6 +1,13 @@
 import { View, Text, SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
+import * as Yup from 'yup'
 
+const passwordSchema = Yup.object().shape({
+    passwordLength: Yup.number()
+    .min(4,'Should be minimum of 4 characters')
+    .max(16,'Should be maximum of 16 characters')
+    .required()
+})
 export default function App3() {
   return (
     <SafeAreaView>
