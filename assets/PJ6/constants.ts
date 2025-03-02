beae75f5a0934384b64e798b9a74ac59905cccc9
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 18:13:38 +0545

    Added contants to the PJ6

---
+++ assets/PJ6/constants.ts
@@ -0,0 +1,57 @@
+export const currencyByRupee: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 0.0072,
+      flag: '🇺🇸',
+      symbol: '$',
+    },
+    {
+      name: 'EURO',
+      value: 0.0069,
+      flag: '🇪🇺',
+      symbol: '€',
+    },
+    {
+      name: 'POUND',
+      value: 0.0057,
+      flag: '🇬🇧',
+      symbol: '£',
+    },
+    {
+      name: 'RUBEL',
+      value: 0.64,
+      flag: '🇷🇺',
+      symbol: '₽',
+    },
+    {
+      name: 'AUS DOLLAR',
+      value: 0.012,
+      flag: '🇦🇺',
+      symbol: 'A$',
+    },
+    {
+      name: 'CAN DOLLAR',
+      value: 0.010,
+      flag: '🇨🇦',
+      symbol: 'C$',
+    },
+    {
+      name: 'YEN',
+      value: 1.08,
+      flag: '🇯🇵',
+      symbol: '¥',
+    },
+    {
+      name: 'DINAR',
+      value: 0.0022,
+      flag: '🇰🇼',
+      symbol: 'KD',
+    },
+    {
+      name: 'BITCOIN',
+      value: 0.00000008,
+      flag: '🎰',
+      symbol: '₿',
+    },
+  ];
+  
