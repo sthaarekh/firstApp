dd6db70e546a2b3f29cb29a8aaf90d5083c8de87
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 14:35:59 +0545

    Roll the dice started

---
+++ App5.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App5() {
+  return (
+    <View>
+      <Text>App5</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import App2 from './App2';
 import App3 from './App3';
-import {name as appName} from './app.json';
 import App4 from './App4';
+import App5 from './App5';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App4);
+AppRegistry.registerComponent(appName, () => App5);
