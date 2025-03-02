75c81d579747e9e997b26f2b25751b6cd5f1970a
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 15:16:18 +0545

    Roll the dice completed

--- PJ5_RollTheDice.tsx
+++ PJ5_RollTheDice.tsx
@@ -1,5 +1,5 @@
-import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
-import { PropsWithChildren } from 'react'
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from 'react-native'
+import { PropsWithChildren, useState } from 'react'
 import React from 'react'
 import DiceOne from './assets/PJ5/One.png'
 import DiceTwo from './assets/PJ5/Two.png'
@@ -20,11 +20,69 @@ const Dice=({imageURL}: DiceProps): JSX.Element=>{
   )
 }
 export default function App5() {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne)
+  const rollDiceOnTap = () =>{
+    let randomNumber = Math.floor(Math.random()*6)+1;
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+      default:
+        break;
+    }
+  }
   return (
-    <View>
-      <Text>App5</Text>
+    <View style={styles.container}>
+      <Dice imageURL={diceImage}/>
+      <Pressable onPress={rollDiceOnTap} style={styles.rollDiceBtn}>
+        <Text style={styles.rollDiceBtnText}>Roll the dice</Text>
+      </Pressable>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  diceImage:{
+    width: 200,
+    height: 200,
+  },
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rollDiceBtn: {
+    backgroundColor: "#4CAF50",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 5, // For Android shadow
+    shadowColor: "#000", // For iOS shadow
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  rollDiceBtnText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+})
