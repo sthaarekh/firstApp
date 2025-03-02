0c91229f6800b568d8ecca7b3826ec181a2d1264
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 18:06:44 +0545

    Currency converter app starting

---
+++ PJ6_CurrencyConverter.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PJ6_CurrencyConverter() {
+  return (
+    <View>
+      <Text>PJ6_CurrencyConverter</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import App2 from './PJ2_Cards';
 import App3 from './PJ3_PasswordGenerator';
 import App4 from './PJ4_RandomBg';
 import App5 from './PJ5_RollTheDice';
+import PJ6_CurrencyConverter from './PJ6_CurrencyConverter';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App5);
+AppRegistry.registerComponent(appName, () => PJ6_CurrencyConverter);
