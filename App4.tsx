d520194a33a43d6d8ebb576d3846fe404c106f13
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 12:22:08 +0545

    App 4 initiated

---
+++ App4.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App4() {
+  return (
+    <View>
+      <Text>App4</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import App from './App';
 import App2 from './App2';
 import App3 from './App3';
 import {name as appName} from './app.json';
+import App4 from './App4';
 
-AppRegistry.registerComponent(appName, () => App3);
+AppRegistry.registerComponent(appName, () => App4);
