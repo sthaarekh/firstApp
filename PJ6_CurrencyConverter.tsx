1f671073260480c0eeff49fa8c357134cd63bc60
sthaarekh <arekhshrestha@gmail.com>
2025-03-02 18:43:08 +0545

    Currecy conversion code completed

--- PJ6_CurrencyConverter.tsx
+++ PJ6_CurrencyConverter.tsx
@@ -1,7 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { currencyByRupee } from './assets/PJ6/constants'
+import CurrencyButton from './assets/PJ6/Components/CurrencyButton'
+import Snackbar from 'react-native-snackbar'
 
 export default function PJ6_CurrencyConverter() {
+    const [inputValue, setInputValue] = useState('')
+    const [resultValue, setResultValue] = useState('')
+    const [targetCurrency, setTargetCurrency] = useState('')
+
+    const buttonPress=(tragetValue: Currency)=>{
+        if(!inputValue){
+            return Snackbar.show({
+                text: "Enter a value to convert.",
+                backgroundColor: "#EA7773",
+                textColor: "#000000"
+            })
+        }
+
+        const inputAmount = parseFloat(inputValue);
+            if(!isNaN(inputAmount)){
+                const convertedValue = inputAmount * tragetValue.value;
+                const result = `${tragetValue.symbol} ${convertedValue.toFixed(2)}`
+                setResultValue(result);
+                setTargetCurrency(tragetValue.name)
+            }  else{
+                return Snackbar.show({
+                    text: "Not a valid number to convert",
+                    backgroundColor: "#F4BE2C",
+                    textColor: "#000000"
+                })
+            }
+        }
+    }
+
   return (
     <View>
       <Text>PJ6_CurrencyConverter</Text>
