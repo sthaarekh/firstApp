e63a31c680b7d24ac43e6acd1cce6d691d0d3980
sthaarekh <arekhshrestha@gmail.com>
2025-03-03 11:59:12 +0545

    Added constatnts

---
+++ assets/PJ7/Constant.ts
@@ -0,0 +1,45 @@
+import { Track } from "react-native-track-player";
+
+export const playListData : Track[] = [
+    {
+        id: 1,
+        title: 'A Aama Jam Bhanyo Malai',
+        artist: 'Rita Thapa Magar',
+        album: 'Personal',
+        artwork:'./artwork/jambhanyo.jpeg',
+        url: './music/A Aama Jam Bhanyo Malai - Rita Thapa Magar Prakash Saput Sanisha Bhattarai New Nepali Song.mp3'
+    },
+    {
+        id: 2,
+        title: 'AJR - The Good Part',
+        artist: 'AJR',
+        album: 'Personal',
+        artwork:'./artwork/ajr.jpeg',
+        url: './music/AJR - The Good Part (Official Video).mp3'
+    },
+    {
+        id: 3,
+        title: 'Meri maya fula jasti',
+        artist: 'Purna Rai',
+        album: 'Sparsha Sangeet',
+        artwork:'./artwork/Sparsha.jpeg',
+        url: './music/Meri maya fula jasti Sparsha Sangeet Purna Rai and dajubhaiharu lyrics video.mp3'
+    },
+    {
+        id: 4,
+        title: 'RAIYA CHANDIKO',
+        artist: 'Bijay Baral, Anjana Baraili',
+        album: 'Purna Bahadur Ko Sarangi',
+        artwork:'./artwork/raiya.jpeg',
+        url: './music/RAIYA CHANDIKO - Purna Bahadur Ko Sarangi - New Dohori Movie Song Ft. Bijay Baral, Anjana Baraili.mp3'
+    },
+    {
+        id: 5,
+        title: 'Tick Tick Boom',
+        artist: 'Sage The Gemini',
+        album: 'Scoob',
+        artwork:'./artwork/scoob.jpeg',
+        url: './music/Tick Tick Boom - Sage The Gemini ft. BygTwo3 (from Scoob The Album) Official Audio 11.51.15 AM.mp3'
+    },
+    
+]
